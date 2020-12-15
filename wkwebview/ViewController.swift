930d87a70d9e77ffcb36635dad672dea66422889
abpa <abhishek@excelsiorstudio.in>
2020-12-15 21:59:33 +0530

    changes for opening url in new tab

--- wkwebview/ViewController.swift
+++ wkwebview/ViewController.swift
@@ -8,18 +8,26 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
     var webView: WKWebView!
 
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
+        self.webView.uiDelegate = self
         view = webView
     }
 
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if navigationAction.targetFrame == nil {
+            webView.load(navigationAction.request)
+        }
+        return nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.google.com")!
+        let url = URL(string: "https://burpple.webflow.io/beyond-mobile-v3")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
